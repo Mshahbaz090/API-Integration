a557a3e9fc8c20fea161bddae1311bc0d1ddd90d
Shabaz123 <shahbaziub1377@gmail.com>
2024-01-11 12:02:26 +0500

    first comit

---
+++ index.js
@@ -0,0 +1,67 @@
+const Ahmed = [
+    {
+        name1: "shahbaz",
+        email: "267@gmail.com"
+    },
+
+
+    {
+        name1: "ali",
+        lication: {
+            city: "lodhran",
+            province: "punjab",
+            country: "pakistan"
+        },
+
+
+        email: "dkjh2@gmail.com",
+        Area: {
+
+            City: "East pakistan",
+            province: "east Pakistan",
+            country: "pakistan",
+        },
+    },
+
+    {
+        name1: "noor",
+        location: {
+            city: "Multan",
+            province: "Punjab",
+            country: "Pakistan"
+        },
+        email: "sdf267@gmail.com"
+    },
+
+    {
+        name1: "fauzan",
+        email: "jkhjh267@gmail.com"
+    },
+
+    {
+        name1: "mnhg",
+        email: "jhkj267@gmail.com"
+    },
+
+    {
+        name1: "sds",
+        email: "wsd267@gmail.com"
+    },
+
+    {
+        name1: "shahbaz",
+        email: "267@gmail.com"
+    },
+
+    {
+        name1: "shahbaz",
+        email: "267@gmail.com"
+    },
+
+    {
+        name1: "shahbaz",
+        email: "267@gmail.com"
+    },
+]
+
+console.log(Ahmed)
